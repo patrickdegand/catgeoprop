f5630d5ff67c5caf68ea5e7f6d716b9e7c8a584e
patrickdegand <pdegand@mailinator.com>
2019-11-28 15:39:00 +0100

    create github pages

--- sw-connect.js
+++ sw-connect.js
@@ -1,2 +1,19 @@
+// This is the service worker with the Advanced caching
 
-"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("sw.js").then(function(a){console.log("ServiceWorker registration successful with scope: ",a.scope)},function(a){console.error("ServiceWorker registration failed: ",a)})});
+// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+
+// Check compatibility for the browser we're running this in
+if ("serviceWorker" in navigator) {
+  if (navigator.serviceWorker.controller) {
+    console.log("[PWA Builder] active service worker found, no need to register");
+  } else {
+    // Register the service worker
+    navigator.serviceWorker
+      .register("sw.js", {
+        scope: "./"
+      })
+      .then(function (reg) {
+        console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+      });
+  }
+}
