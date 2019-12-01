92634bfbfd45025a141aed567c27747c114c3570
patrickdegand <pdegand@mailinator.com>
2019-12-01 09:49:49 +0100

    create github pages

--- sw-connect.js
+++ sw-connect.js
@@ -1,7 +1,14 @@
-"serviceWorker" in navigator && window.addEventListener("load", function() {
-    navigator.serviceWorker.register("/catgeoprop/sw.js").then(function(a) {
-        console.log("ServiceWorker registration successful with scope: ", a.scope)
-    }, function(a) {
-        console.error("ServiceWorker registration failed: ", a)
-    })
-});
+if ("serviceWorker" in navigator) {
+  if (navigator.serviceWorker.controller) {
+    console.log("[PWA Builder] active service worker found, no need to register");
+  } else {
+    // Register the service worker
+    navigator.serviceWorker
+      .register("pwabuilder-sw.js", {
+        scope: "./"
+      })
+      .then(function (reg) {
+        console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+      });
+  }
+}
