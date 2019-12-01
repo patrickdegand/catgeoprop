b8256a08d88de6ea79b4d8209a58cf58dcc8a697
patrickdegand <pdegand@mailinator.com>
2019-12-01 09:04:48 +0100

    create github pages

--- sw-connect.js
+++ sw-connect.js
@@ -1,7 +1,7 @@
 "serviceWorker" in navigator && window.addEventListener("load", function() {
     navigator.serviceWorker.register("sw.js").then(function(a) {
-        alert("ServiceWorker registration successful with scope: ", a.scope)
+        console.log("ServiceWorker registration successful with scope: ", a.scope)
     }, function(a) {
-        alert("ServiceWorker registration failed: ", a)
+        console.error("ServiceWorker registration failed: ", a)
     })
 });
